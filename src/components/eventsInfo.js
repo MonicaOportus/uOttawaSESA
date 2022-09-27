62cba70a0126aa30707960b71e7fa3e68d25ced5
Damian Oportus <coportus99@gmail.com>
2022-09-27 11:57:47 -0400

    Update event

--- src/components/eventsInfo.js
+++ src/components/eventsInfo.js
@@ -3,7 +3,7 @@ const eventInfo = [
     id: 1,
     date: "September 28st, 2022",
     title: "Meet and greet",
-    location: "To be determined @ 7:30 pm",
+    location: "Morisset 250 uOttawa @ 7:30 pm",
     invitation:
       "Come meet your fellow software engineering students and get to know the SESA execs! This is a great opportunity to get involved with the association and learn about what we do. We'll be announcing more details soon, so stay tuned!",
     instaLink: "https://www.instagram.com/p/CitT3EIOVLD/?hl=en",
