267522192e6ef3d54afb16d81bdca612d2161875
Damian Oportus <coportus99@gmail.com>
2022-10-06 11:39:05 -0400

    Ciena event

--- src/components/eventsInfo.js
+++ src/components/eventsInfo.js
@@ -1,21 +1,22 @@
 const eventInfo = [
   {
     id: 1,
+    date: "October 10th, 2022",
+    title: "Ciena Social",
+    location: "STEM building #117 @ 6 to 8pm",
+    invitation:
+      "Find your new grad/CO-OP job at Ciena! Network with representatives from their company, and learn more about the company's projects. Register",
+    instaLink: "https://www.eventbrite.com/e/ciena-social-tickets-427567384557",
+  },
+
+  {
+    id: 2,
     date: "September 28st, 2022",
     title: "Meet and greet",
     location: "Morisset 250 uOttawa @ 7:30 pm",
     invitation:
       "Come meet your fellow software engineering students and get to know the SESA execs! This is a great opportunity to get involved with the association and learn about what we do. We'll be announcing more details soon, so stay tuned!",
     instaLink: "https://www.instagram.com/p/CitT3EIOVLD/?hl=en",
-  },
-
-  {
-    id: 2,
-    date: "September 21st, 2022",
-    title: "SESA Icebreaker",
-    location: "CRX 421",
-    invitation:
-      "Come meet the SESA team and your fellow software engineers at SESA's first event in two and a half years! Snacks and refreshments provided.",
   },
   {
     id: 3,
