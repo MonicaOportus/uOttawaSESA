19e06a490db5b426ad3fe4e8f1c564f94f352231
Damian Oportus <coportus99@gmail.com>
2022-10-06 12:57:06 -0400

    DiscordLink invite

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,126 +1,126 @@
 import logo from "../images/logo.png";
 // import { Menu } from '@headlessui/react';
 
-function Navbar () {
+function Navbar() {
+  // function MyDropdown() {
+  //   return (
+  //     <Menu>
+  //         <div className="absolute lg:hidden w-[100%]">
+  //             <Menu.Button><i className="fa-solid fa-2xl fa-bars absolute right-[50px]"></i></Menu.Button>
+  //             <Menu.Items className="list-none flex flex-col bg-white mt-[25px]">
+  //                 <Menu.Item className="py-4 navItem">
+  //                 {({ active }) => (
+  //                     <a
+  //                     className={`${active}`}
+  //                     href="/about"
+  //                     >
+  //                     About
+  //                     </a>
+  //                 )}
+  //                 </Menu.Item>
+  //                 <Menu.Item className="py-4 navItem">
+  //                 {({ active }) => (
+  //                     <a
+  //                     className={`${active}`}
+  //                     href="#events"
+  //                     >
+  //                     Events
+  //                     </a>
+  //                 )}
+  //                 </Menu.Item>
+  //                 <Menu.Item className="py-4 navItem">
+  //                 {({ active }) => (
+  //                     <a
+  //                     className={`${active}`}
+  //                     href="/resources"
+  //                     >
+  //                     Resources
+  //                     </a>
+  //                 )}
+  //                 </Menu.Item>
+  //                 <Menu.Item className="py-4 navItem">
+  //                 {({ active }) => (
+  //                     <a
+  //                     className={`${active}`}
+  //                     href="/partners"
+  //                     >
+  //                     Partners
+  //                     </a>
+  //                 )}
+  //                 </Menu.Item>
+  //                 <Menu.Item className="py-4 navItem">
+  //                 {({ active }) => (
+  //                     <a
+  //                     className={`${active}`}
+  //                     href="/contact"
+  //                     >
+  //                     Contact
+  //                     </a>
+  //                 )}
+  //                 </Menu.Item>
+  //             </Menu.Items>
+  //         </div>
+  //     </Menu>
+  //   )
+  // }
 
-    // function MyDropdown() {
-    //   return (
-    //     <Menu>
-    //         <div className="absolute lg:hidden w-[100%]">
-    //             <Menu.Button><i className="fa-solid fa-2xl fa-bars absolute right-[50px]"></i></Menu.Button>
-    //             <Menu.Items className="list-none flex flex-col bg-white mt-[25px]">
-    //                 <Menu.Item className="py-4 navItem">
-    //                 {({ active }) => (
-    //                     <a
-    //                     className={`${active}`}
-    //                     href="/about"
-    //                     >
-    //                     About
-    //                     </a>
-    //                 )}
-    //                 </Menu.Item>
-    //                 <Menu.Item className="py-4 navItem">
-    //                 {({ active }) => (
-    //                     <a
-    //                     className={`${active}`}
-    //                     href="#events"
-    //                     >
-    //                     Events
-    //                     </a>
-    //                 )}
-    //                 </Menu.Item>
-    //                 <Menu.Item className="py-4 navItem">
-    //                 {({ active }) => (
-    //                     <a
-    //                     className={`${active}`}
-    //                     href="/resources"
-    //                     >
-    //                     Resources
-    //                     </a>
-    //                 )}
-    //                 </Menu.Item>
-    //                 <Menu.Item className="py-4 navItem">
-    //                 {({ active }) => (
-    //                     <a
-    //                     className={`${active}`}
-    //                     href="/partners"
-    //                     >
-    //                     Partners
-    //                     </a>
-    //                 )}
-    //                 </Menu.Item>
-    //                 <Menu.Item className="py-4 navItem">
-    //                 {({ active }) => (
-    //                     <a
-    //                     className={`${active}`}
-    //                     href="/contact"
-    //                     >
-    //                     Contact
-    //                     </a>
-    //                 )}
-    //                 </Menu.Item>
-    //             </Menu.Items>
-    //         </div>
-    //     </Menu>
-    //   )
-    // }
-
-    return (
-        <div className="pb-5">
-
-            <div className="flex items-center justify-between pt-7 sticky"> {/* When you remove the icons and 
+  return (
+    <div className="pb-5">
+      <div className="flex items-center justify-between pt-7 sticky">
+        {" "}
+        {/* When you remove the icons and 
             add unecomment the pages change "flex" to "md:flex" and remove ... */}
-                {/* <MyDropdown/> */}
-                <a href="/">
-                    <img 
-                        src={logo} 
-                        alt="logo" 
-                        className="w-[110px] ml-[25px] md:ml-[110px] md:w-[151px] xl:ml-[210px]"
-                    />
-                </a>
-                <div className="flex gap-4 mr-[15px] md:mr-[100px] lg:mr-[200px]">
-                    <a 
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://www.instagram.com/uottawasesa/"
-                        className="hover:scale-[1.05]"
-                    >
-                        <i className="fa-xl fa-brands fa-instagram"></i>
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://www.linkedin.com/company/software-engineering-student-association/"
-                        className="hover:scale-[1.05]"
-                    >
-                        <i className="fa-xl fa-brands fa-linkedin"></i>
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="/"
-                        className="hover:scale-[1.05]"
-                    >
-                        <i className="fa-xl fa-brands fa-discord"></i>
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://www.facebook.com/UOttawaSESA"
-                        className="hover:scale-[1.05]"
-                    >
-                        <i className="fa-xl fa-brands fa-square-facebook"></i>
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="/"
-                        className="hover:scale-[1.05]"
-                    >
-                        <i className="fa-xl fa-brands fa-github"></i>
-                    </a>
-                </div> 
-                {/* <div 
+        {/* <MyDropdown/> */}
+        <a href="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[110px] ml-[25px] md:ml-[110px] md:w-[151px] xl:ml-[210px]"
+          />
+        </a>
+        <div className="flex gap-4 mr-[15px] md:mr-[100px] lg:mr-[200px]">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/uottawasesa/"
+            className="hover:scale-[1.05]"
+          >
+            <i className="fa-xl fa-brands fa-instagram"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/company/software-engineering-student-association/"
+            className="hover:scale-[1.05]"
+          >
+            <i className="fa-xl fa-brands fa-linkedin"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://discord.gg/xaQwdX8gyx"
+            className="hover:scale-[1.05]"
+          >
+            <i className="fa-xl fa-brands fa-discord"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.facebook.com/UOttawaSESA"
+            className="hover:scale-[1.05]"
+          >
+            <i className="fa-xl fa-brands fa-square-facebook"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/DemianOportus/SESA"
+            className="hover:scale-[1.05]"
+          >
+            <i className="fa-xl fa-brands fa-github"></i>
+          </a>
+        </div>
+        {/* <div 
                     className="hidden myNav flex-shrink mx-4 lg:flex list-none">
                     <a href="/about">
                         <li className="navItem">
@@ -148,9 +148,9 @@ function Navbar () {
                         </li>
                     </a>
                 </div> */}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
