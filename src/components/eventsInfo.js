1ad6ff2835330c436bebb3d4c20c8fd082d71c00
Damian Oportus <coportus99@gmail.com>
2022-11-21 15:28:18 -0500

    Event update

--- src/components/eventsInfo.js
+++ src/components/eventsInfo.js
@@ -1,21 +1,21 @@
 const eventInfo = [
   {
     id: 1,
+    date: "Nov 24th, 3:00-4:00pm",
+    title: "Brown Bag Lecture",
+    location: "STEM 117",
+    invitation:
+      "Looking ahead to your first co-op? Trying to build your own product? Knowing how to manage your project efficiently can take your software development to the next level! ",
+    instaLink: "https://www.instagram.com/p/ClO6nO8puSu/?hl=en",
+  },
+  {
+    id: 2,
     date: "February, 2023",
     title: "Hack the Hill",
     location: "TBD",
     invitation:
       "The uOttawa Engineering Student Society, IEEE uOttawa Student Branch, Computer Science Student Association and Software Engineering Student Association are proud to bring a flagship hackathon to Ottawa where students can network and compete with the best!",
     instaLink: "https://www.instagram.com/p/CkYl9sOuUe3/?hl=en",
-  },
-
-  {
-    id: 2,
-    date: "",
-    title: "",
-    location: "More events are coming!",
-    invitation: "Follow us on social media for updates: ",
-    instaLink: "https://www.instagram.com/p/CitT3EIOVLD/?hl=en",
   },
 
   {
