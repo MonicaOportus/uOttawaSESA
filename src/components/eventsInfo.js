1dbd586588c473a163605351fe230c4db4ee5b09
Damian Oportus <coportus99@gmail.com>
2023-01-25 10:05:19 -0500

    New event

--- src/components/eventsInfo.js
+++ src/components/eventsInfo.js
@@ -1,5 +1,15 @@
 const eventInfo = [
   {
+    id: 2,
+    date: "February 3 at 12:00 pm",
+    title: "Code && Coffee",
+    location: "LMX 241",
+    invitation:
+      "Join for a workshop on the intro to React and enjoy some delicious coffee☕, tea🍵 and pizza🍕, while you learn and network with your peers. ",
+    instaLink: "https://discord.gg/f2KDfCDN?event=1067794164042960916",
+  },
+
+  {
     id: 1,
     date: "3-5 February, 2023",
     title: "Hack the Hill",
@@ -9,14 +19,6 @@ const eventInfo = [
     instaLink: "https://www.instagram.com/p/CkYl9sOuUe3/?hl=en",
   },
 
-  {
-    id: 2,
-    date: "",
-    title: "",
-    location: "More events are coming!",
-    invitation: "Follow us on social media for updates: ",
-    instaLink: "https://www.instagram.com/p/CitT3EIOVLD/?hl=en",
-  },
   {
     id: 3,
     date: "",
