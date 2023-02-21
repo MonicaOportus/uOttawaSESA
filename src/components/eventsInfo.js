cd0271b54dc197a8d7b1a2d7ef8ab0b9befbe202
Damian Oportus <coportus99@gmail.com>
2023-02-20 19:22:52 -0500

    New event

--- src/components/eventsInfo.js
+++ src/components/eventsInfo.js
@@ -1,24 +1,21 @@
 const eventInfo = [
   {
-    id: 2,
-    date: "February 3 at 12:00 pm",
-    title: "Code && Coffee",
-    location: "LMX 241",
-    invitation:
-      "Join for a workshop on the intro to React and enjoy some delicious coffee☕, tea🍵 and pizza🍕, while you learn and network with your peers. ",
-    instaLink: "https://discord.gg/f2KDfCDN?event=1067794164042960916",
-  },
-
-  {
     id: 1,
-    date: "3-5 February, 2023",
+    date: "3-5 March, 2023",
     title: "Hack the Hill",
     location: "uOttawa Campus",
     invitation:
       "The uOttawa Engineering Student Society, IEEE uOttawa Student Branch, Computer Science Student Association and Software Engineering Student Association are proud to bring a flagship hackathon to Ottawa where students can network and compete with the best!",
     instaLink: "https://www.instagram.com/p/CkYl9sOuUe3/?hl=en",
   },
-
+  {
+    id: 2,
+    date: "",
+    title: "",
+    location: "More events are coming!",
+    invitation: "Follow us on social media for updates: ",
+    instaLink: "https://www.instagram.com/p/CitT3EIOVLD/?hl=en",
+  },
   {
     id: 3,
     date: "",
